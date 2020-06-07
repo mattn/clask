db9c1849d1a3d360f98083b0ec93ce2fb1b6fccd
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2020-06-07 22:54:56 +0900

    Not Found

--- clask/core.hpp
+++ clask/core.hpp
@@ -83,13 +83,76 @@ logger::~logger() {
 
 typedef std::pair<std::string, std::string> header;
 
+static std::map<int, std::string> status_codes = {
+  { 100, "Continue" },
+  { 101, "Switching Protocols" },
+  { 102, "Processing" },
+  { 200, "OK" },
+  { 201, "Created" },
+  { 202, "Accepted" },
+  { 203, "Non-Authoritative Information" },
+  { 204, "No Content" },
+  { 205, "Reset Content" },
+  { 206, "Partial Content" },
+  { 207, "Multi-Status" },
+  { 208, "Already Reported" },
+  { 300, "Multiple Choices" },
+  { 301, "Moved Permanently" },
+  { 302, "Found" },
+  { 303, "See Other" },
+  { 304, "Not Modified" },
+  { 305, "Use Proxy" },
+  { 307, "Temporary Redirect" },
+  { 400, "Bad Request" },
+  { 401, "Unauthorized" },
+  { 402, "Payment Required" },
+  { 403, "Forbidden" },
+  { 404, "Not Found" },
+  { 405, "Method Not Allowed" },
+  { 406, "Not Acceptable" },
+  { 407, "Proxy Authentication Required" },
+  { 408, "Request Timeout" },
+  { 409, "Conflict" },
+  { 410, "Gone" },
+  { 411, "Length Required" },
+  { 412, "Precondition Failed" },
+  { 413, "Request Entity Too Large" },
+  { 414, "Request-URI Too Large" },
+  { 415, "Unsupported Media Type" },
+  { 416, "Request Range Not Satisfiable" },
+  { 417, "Expectation Failed" },
+  { 418, "I""m a teapot" },
+  { 422, "Unprocessable Entity" },
+  { 423, "Locked" },
+  { 424, "Failed Dependency" },
+  { 425, "No code" },
+  { 426, "Upgrade Required" },
+  { 428, "Precondition Required" },
+  { 429, "Too Many Requests" },
+  { 431, "Request Header Fields Too Large" },
+  { 449, "Retry with" },
+  { 500, "Internal Server Error" },
+  { 501, "Not Implemented" },
+  { 502, "Bad Gateway" },
+  { 503, "Service Unavailable" },
+  { 504, "Gateway Timeout" },
+  { 505, "HTTP Version Not Supported" },
+  { 506, "Variant Also Negotiates" },
+  { 507, "Insufficient Storage" },
+  { 509, "Bandwidth Limit Exceeded" },
+  { 510, "Not Extended" },
+  { 511, "Network Authentication Required" },
+};
+
 class response {
 private:
+  int code;
   std::vector<header> headers;
   bool header_out;
   int s;
 public:
-  response(int s) : s(s), header_out(false) { }
+  response(int s, int code) : s(s), code(code), header_out(false) { }
+  void set_status(int);
   void set_header(std::string, std::string);
   void write(std::string);
 };
@@ -101,7 +164,9 @@ void response::set_header(std::string key, std::string value) {
 void response::write(std::string content) {
   if (!header_out) {
     header_out = true;
-    std::string res_headers = "HTTP/1.0 200 OK\r\n";
+    std::ostringstream os;
+    os << "HTTP/1.0 " << code << status_codes[code] << "\r\n";
+    std::string res_headers = os.str();
     send(s, res_headers.data(), res_headers.size(), 0);
     for (auto h : headers) {
       auto hh = h.first + ": " + h.second + "\r\n";
@@ -278,14 +343,14 @@ void server_t::run() {
       for (auto h : handlers) {
         if (h.first == req_path) {
           found = true;
-          response res(s);
+          response res(s, 200);
           h.second(res, req);
           break;
         }
       }
     }
     if (!found) {
-      std::string res_headers = "HTTP/1.0 404 Not Found\r\nContent-Type: text/plain\r\n\r\nnot found";
+      std::string res_headers = "HTTP/1.0 404 Not Found\r\nContent-Type: text/plain\r\n\r\nNot Found";
       send(s, res_headers.data(), res_headers.size(), 0);
     }
     closesocket(s);
--- main.cxx
+++ main.cxx
@@ -5,6 +5,11 @@ int main() {
   s.GET("/", [](clask::request& req) {
     return "OK!";
   });
+  s.GET("/notfound", [](clask::response& resp, clask::request& req) {
+    resp.set_status(404);
+    resp.set_header("content-type", "text/html");
+    resp.write("Not Found");
+  });
   s.GET("/foo", [](clask::response& resp, clask::request& req) {
     resp.set_header("content-type", "text/html");
     resp.write("he<b>l</b>lo");
