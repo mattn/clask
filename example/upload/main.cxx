a13bc6937a572f1fe71665f19dcabe4663bddf86
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2020-06-15 00:54:59 +0900

    Fix example

--- example/upload/main.cxx
+++ example/upload/main.cxx
@@ -1,4 +1,5 @@
 #include <clask/core.hpp>
+#include <nlohmann/json.hpp>
 
 static void save_file(clask::part p) {
   auto filename = p.filename();
@@ -13,7 +14,18 @@ static void save_file(clask::part p) {
 int main() {
   auto s = clask::server();
   s.log.default_level = clask::INFO;
+  s.static_dir("/files", "./files");
   s.static_dir("/", "./public");
+  s.GET("/api", [](clask::request& req) {
+    nlohmann::json data;
+    int n = 0;
+    for (const auto& e : std::filesystem::directory_iterator("files")) {
+      auto fn = e.path().filename().u8string();
+      if (fn[0] == '.') continue;
+	  data[n++] = fn;
+    }
+	return data.dump();
+  });
   s.POST("/upload", [](clask::request& req) -> clask::response {
     std::vector<clask::part> parts;
     if (!req.parse_multipart(parts) || parts.size() == 0) {
