3e6689b02bcfe85375fde82efa04c6bbd23cff13
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2020-06-14 22:07:36 +0900

    Use clask::to_wstring

--- example/upload/main.cxx
+++ example/upload/main.cxx
@@ -1,26 +1,10 @@
 #include <clask/core.hpp>
 
-static std::wstring convert(const std::string& input) {
-  try {
-    std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
-    return converter.from_bytes(input);
-  } catch (std::range_error& e) {
-    size_t length = input.length();
-    std::wstring result;
-    result.reserve(length);
-    for (size_t i = 0; i < length; i++) {
-      result.push_back(input[i] & 0xFF);
-    }
-    return result;
-  }
-}
-
 static void save_file(clask::part p) {
   auto filename = p.filename();
   if (filename.empty()) return;
-  std::wstring wfn = convert(filename);
-  std::filesystem::path fn(wfn);
-  wfn = L"download/" + fn.filename().wstring();
+  std::filesystem::path fn(clask::to_wstring(filename));
+  std::wstring wfn = L"download/" + fn.filename().wstring();
   std::ofstream out(wfn.c_str(), std::ios::out | std::ios::binary);
   out << p.body;
   out.close();
