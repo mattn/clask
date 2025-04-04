dd6ecc4af2f4d43e048be7843b9651977c9344f5
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2025-04-04 22:40:14 +0900

    fix

--- example/file/main.cxx
+++ example/file/main.cxx
@@ -1,8 +1,17 @@
 #include <clask/core.hpp>
+#include <argparse/argparse.hpp>
 
-int main() {
+int main(int argc, char* argv[]) {
+  argparse::ArgumentParser program("file", "0.0.0");
+  program.add_argument("-dir")
+      .default_value("./public")
+      .help("public directory")
+      .metavar("PUBLIC_DIR")
+      .nargs(1);
+  program.parse_args(argc, argv);
+
   auto s = clask::server();
   s.log.default_level = clask::log_level::INFO;
-  s.static_dir("/", "./public");
+  s.static_dir("/", program.get<std::string>("-dir"));
   s.run();
 }
