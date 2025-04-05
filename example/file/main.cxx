a23e98c8cefbf54fb65c3aa68e081de38069336a
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2025-04-06 00:39:26 +0900

    -addr for example-file

--- example/file/main.cxx
+++ example/file/main.cxx
@@ -1,5 +1,6 @@
 #include <clask/core.hpp>
 #include <argparse/argparse.hpp>
+#include <iostream>
 
 int main(int argc, char* argv[]) {
   argparse::ArgumentParser program("file", "0.0.0");
@@ -8,10 +9,18 @@ int main(int argc, char* argv[]) {
       .help("public directory")
       .metavar("PUBLIC_DIR")
       .nargs(1);
+  program.add_argument("-addr")
+      .default_value("0.0.0.0:8080")
+      .help("server address")
+      .metavar("ADDR")
+      .nargs(1);
   program.parse_args(argc, argv);
 
   auto s = clask::server();
   s.log.default_level = clask::log_level::INFO;
   s.static_dir("/", program.get<std::string>("-dir"));
-  s.run();
+
+  auto addr = program.get<std::string>("-addr");
+  std::cerr << "started " << addr << std::endl;
+  s.run(addr);
 }
